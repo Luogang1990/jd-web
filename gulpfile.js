1a71fb64b9b9652f79a668eaa2108b8831f7f9f1
wangluogang <461251455@qq.com>
2017-05-10 21:12:27 +0800

    #新增功能

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,21 @@
  * Created by Administrator on 2017/5/9.
  */
 var gulp=require("gulp");
- gulp.task("show",function(){
-     console.log("hello");
+var less=require("less");
+var $ = require('gulp-load-plugins')();//加载插件
+/*把less文件编译成CSS*/
+ gulp.task("less",function(){
+     return gulp.src("APP/styles/*.less")
+         .pipe($.less())
+         .pipe(gulp.dest("dist/css/"));
  })
+gulp.task("dist",function(){
+    gulp.watch("APP/styles/*.less",["less"])
+})
+/*压缩图片*/
+gulp.task('images', function() {
+    return gulp.src('../img/*')
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(notify({ message: 'Images task complete' }));
+});
