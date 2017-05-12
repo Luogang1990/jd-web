3b8d50c9962c1c8745c83a192875f5d04383b041
wangluogang <461251455@qq.com>
2017-05-12 15:16:10 +0800

    完成首页！

--- APP/scripts/index.js
+++ APP/scripts/index.js
@@ -1,38 +1,36 @@
-function scroll(){
-    if(window.pageYOffset!=null){
-        return{
-            left:window.pageXOffset,
-            top:window.pageYOffset
+function scroll() {
+    //判断当前的浏览器是否是IE9+ 或者其他浏览器
+    if (window.pageYOffset != null) {
+        return {
+            left: window.pageXOffset,
+            top: window.pageYOffset
         };
     }
-    else if(document.compatMode=="CSS1Compat"){
-        return{
-            left:document.documentElement.scrollLeft,
-            top:document.documentElement.scrollTop
+    //检测是否是怪异模式浏览器，就是没有声明<!DOCTYPE html>
+    else if(document.compatMode == "CSS1Compat"){
+        //正常模式
+        return {
+            left: document.documentElement.scrollLeft,
+            top: document.documentElement.scrollTop
         };
     }
-    return{
+    //怪异浏览器
+    return {
         left:document.body.scrollLeft,
         top:document.body.scrollTop
     };
 }
 
-
-document.onscroll=function(){
-    var top=scroll().top;
-    var ss=document.getElementById("");
-    if(top>=300){
-        div.className="div_left fixed_left";
-    }else{
-        div.className="div_left";
-    }
-}
 window.onscroll=function(){
-    var top=scroll().top;
-    var ss=document.getElementById("sousuo2");
-    if(top>=600){
-        ss.className=" sousuo2 fixed_sousuo2";
+    var tt=scroll().top;
+    //var ss=document.getElementById("top");
+    if(tt>=100){
+        var ss=document.getElementById("top");
+        ss.className=" top top-bgc";
     }else{
-        ss.className="sousuo2";
+        var ss=document.getElementById("top");
+        ss.className="top";
     }
 }
+
+
